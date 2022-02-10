d0387083c95d706f3f0efa9accdd4de113e1a789
js988174 <76925402+js988174@users.noreply.github.com>
2022-02-10 22:28:16 +0900

    member 수정4

--- src/main/java/com/spring/blog/blog/dao/MemberDao.java
+++ src/main/java/com/spring/blog/blog/dao/MemberDao.java
@@ -1,9 +1,11 @@
 package com.spring.blog.blog.dao;
 
 import com.spring.blog.blog.domain.Member;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface MemberDao {
     int insert(Member member) throws Exception;
 
