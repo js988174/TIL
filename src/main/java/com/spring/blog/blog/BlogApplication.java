7b366756439fe82ceaa6dca6c2e86f1db8ffb2d4
js988174 <76925402+js988174@users.noreply.github.com>
2022-02-12 20:41:13 +0900

    member 수정7

--- src/main/java/com/spring/blog/blog/BlogApplication.java
+++ src/main/java/com/spring/blog/blog/BlogApplication.java
@@ -3,7 +3,7 @@ package com.spring.blog.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.spring.blog.blog.dao"})
 public class BlogApplication {
 
 	public static void main(String[] args) {
