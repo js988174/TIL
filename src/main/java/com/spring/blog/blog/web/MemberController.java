7bf601e7ab41d8da27acc937db393837f1d0470c
js988174 <76925402+js988174@users.noreply.github.com>
2022-02-12 21:03:26 +0900

    member 수정8

--- src/main/java/com/spring/blog/blog/web/MemberController.java
+++ src/main/java/com/spring/blog/blog/web/MemberController.java
@@ -14,7 +14,7 @@ import javax.servlet.ServletContext;
 import java.util.List;
 
 @Controller
-@RequestMapping("/member/")
+@RequestMapping("/member")
 public class MemberController {
 
     @Autowired
@@ -27,22 +27,23 @@ public class MemberController {
         this.sc = sc;
     }
 
-    @GetMapping("/member/add")
+    @GetMapping("/add")
     public String form()  {
         return "member/form";
     }
 
-    @PostMapping("member/add")
+    @PostMapping("add")
     public String add(Member m) throws Exception {
         memberService.add(m);
         return "redirect:/";
     }
 
 
-    @GetMapping("/member/list")
-    public void list(Model model) throws Exception {
+    @GetMapping("/list")
+    public String list(Model model) throws Exception {
         List<Member> list = memberService.list();
         model.addAttribute("list", list);
+        return "member/memberList";
     }
 
 
