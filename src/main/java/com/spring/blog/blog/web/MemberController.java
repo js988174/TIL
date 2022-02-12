5a3d25ae991354d9496b9774131e6fbbdbf9f699
js988174 <76925402+js988174@users.noreply.github.com>
2022-02-12 21:04:02 +0900

    member 수정8

--- src/main/java/com/spring/blog/blog/web/MemberController.java
+++ src/main/java/com/spring/blog/blog/web/MemberController.java
@@ -5,11 +5,13 @@ import com.spring.blog.blog.domain.Member;
 import com.spring.blog.blog.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletContext;
+import java.util.List;
 
 @Controller
 @RequestMapping("/member")
