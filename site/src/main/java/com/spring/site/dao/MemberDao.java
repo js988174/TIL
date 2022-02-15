5f20aebf8d5c014f49b5e49f1c917935518c5d8e
석성희 <sunghee119z@naver.com>
2022-02-15 23:28:34 +0900

    aa
    aa

--- site/src/main/java/com/spring/site/dao/MemberDao.java
+++ site/src/main/java/com/spring/site/dao/MemberDao.java
@@ -3,6 +3,7 @@ package com.spring.site.dao;
 
 
 import com.spring.site.domain.Member;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,6 +13,8 @@ import java.util.List;
 
 @Mapper
 public interface MemberDao {
+
+    @Insert("INSERT INTO VALUES(#{id}, #{pw}, ${name}")
     int insert(Member member) throws Exception;
     @Select("SELECT * FROM member")
     List<Member> AllList() throws Exception;
