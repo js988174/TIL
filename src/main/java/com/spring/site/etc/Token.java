39d7b8652107e41b6ddd6f9e627140fff0064f7b
js988174 <76925402+js988174@users.noreply.github.com>
2022-02-19 22:13:06 +0900

    .

--- src/main/java/com/spring/site/etc/Token.java
+++ src/main/java/com/spring/site/etc/Token.java
@@ -1,20 +1,30 @@
 package com.spring.site.etc;
 
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.spring.site.mapper.MemberMapper;
+import com.spring.site.service.MemberService;
+import com.spring.site.service.impl.MemberServiceImpl;
+import io.jsonwebtoken.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.xml.crypto.Data;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.Date;
 
 public class Token {
 
-    private String type = "Bearer";
+    private String type = "X-AUTH-TOKEN";
+    private String secretKey = "webfirewood";
 
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    // 토큰 정보
     public String JwtToken() {
         Date now = new Date();
-        System.out.println("token");
 
         return type + Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
@@ -26,5 +36,27 @@ public class Token {
                 .signWith(SignatureAlgorithm.HS256, "secret")
                 .compact();
     }
+
+//    // 토큰 조회
+//    public Authentication getAuthentication(String token) {
+//        UserDetails userDetails = MemberMapper.(this.getUserPk(token));
+//        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+//    }
+
+    // 헤더에서 토큰값 가져오가
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    // 토큰 유효성 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 
 }
--- src/main/java/com/spring/site/etc/TokenFilter.java
+++ src/main/java/com/spring/site/etc/TokenFilter.java
@@ -1,2 +1,28 @@
-package com.spring.site.etc;public class TokenFilter {
+package com.spring.site.etc;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class TokenFilter extends GenericFilterBean {
+
+    private Token jwtToken;
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        String token = jwtToken.resolveToken((HttpServletRequest) request);
+
+        if (token != null && jwtToken.validateToken(token)) {
+//            Authentication authentication = jwtToken.
+        }
+
+    }
 }
