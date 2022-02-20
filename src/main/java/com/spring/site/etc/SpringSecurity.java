177e01a8da17a9417eb2c4214d8c574c332b09d7
석성희 <sunghee119z@naver.com>
2022-02-20 20:42:18 +0900

    asdas
    dasdasdasd

--- src/main/java/com/spring/site/etc/SpringSecurity.java
+++ src/main/java/com/spring/site/etc/SpringSecurity.java
@@ -1,8 +1,10 @@
 package com.spring.site.etc;
 
+import com.spring.site.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,14 +13,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
+    private MemberService memberService;
+
+
+    /* static 관련설정은 무시 */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web .ignoring().antMatchers(
+            "/css/**", "/js/**", "/img/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests() // 6
-                .antMatchers("/member/login", "/member/add").permitAll() // 누구나 접근 허용
-                .antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
-                .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
+                .authorizeRequests() //
+                .antMatchers("/member/login", "/member/add","/").permitAll() // 누구나 접근 허용
+                .antMatchers("/member").hasRole("USER") // USER, ADMIN만 접근 가능
+                .antMatchers("/admin","/member/list").hasRole("ADMIN") // ADMIN만 접근 가능
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
                 .formLogin() // 7
