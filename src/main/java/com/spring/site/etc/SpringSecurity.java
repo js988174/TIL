62679f576431a4946d4eb3fb9b72471d2e86f6b0
석성희 <sunghee119z@naver.com>
2022-02-20 20:17:12 +0900

    aaa
    aaa

--- src/main/java/com/spring/site/etc/SpringSecurity.java
+++ src/main/java/com/spring/site/etc/SpringSecurity.java
@@ -15,16 +15,20 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/").permitAll()    // LoadBalancer Chk
-                .anyRequest().authenticated()
+                .authorizeRequests() // 6
+                .antMatchers("/member/login", "/member/add").permitAll() // 누구나 접근 허용
+                .antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
+                .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
+                .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
-                .formLogin()
-                .defaultSuccessUrl("/member/login", true)
-                .permitAll()
+                .formLogin() // 7
+                .loginPage("/member/login") // 로그인 페이지 링크
+                .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소
                 .and()
-                .logout();
-
+                .logout() // 8
+                .logoutSuccessUrl("/") // 로그아웃 성공시 리다이렉트 주소
+                .invalidateHttpSession(true) // 세션 날리기
+        ;
 
         System.out.println("세큐리티 컨피규어 로그");
     }
