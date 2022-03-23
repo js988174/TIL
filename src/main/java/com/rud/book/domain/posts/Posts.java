ab6b64e6dfa5aa474d7d69f399951f4798b146c7
js988174 <76925402+js988174@users.noreply.github.com>
2022-03-23 22:08:04 +0900

    추가

--- src/main/java/com/rud/book/domain/posts/Posts.java
+++ src/main/java/com/rud/book/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.rud.book.domain.posts;
 
+import com.rud.book.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Getter
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/rud/book/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/rud/book/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootTest
@@ -39,5 +41,24 @@ public class PostsRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
 
+    }
+
+    @Test
+    public void BaseTimeEntity_add() {
+        LocalDateTime now = LocalDateTime.of(2022,6,4,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>> createDate="+posts.getCreateDate()+", modifiedDate =" +posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
