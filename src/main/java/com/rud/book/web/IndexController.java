e8dca325482ebac126663a317e665ddd5f45548b
js988174 <76925402+js988174@users.noreply.github.com>
2022-03-25 23:21:08 +0900

    board 삭제

--- src/main/java/com/rud/book/web/IndexController.java
+++ src/main/java/com/rud/book/web/IndexController.java
@@ -5,6 +5,7 @@ import com.rud.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -29,7 +30,8 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
-
         return "posts-update";
     }
+
+
 }
