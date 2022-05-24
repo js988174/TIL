4b8f57099132785c84cde322869cc297716cd2f7
js988174 <76925402+js988174@users.noreply.github.com>
2022-05-25 02:02:13 +0900

    엔티티

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,20 +1,19 @@
 package jpabook.jpashop;
 
-import org.apache.juli.logging.Log;
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.transaction.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 @ExtendWith({SpringExtension.class})
+@Rollback(value = false)
 public class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
