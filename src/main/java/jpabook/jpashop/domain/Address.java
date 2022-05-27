596442c92a4b0175edc4237fd84e3809bb14cd77
js988174 <76925402+js988174@users.noreply.github.com>
2022-05-27 18:38:00 +0900

    html 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
 public class Address {
 
     private String city;
