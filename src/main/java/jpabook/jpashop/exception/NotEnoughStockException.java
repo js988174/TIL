2dd79314d215c9bba6502cf28bbba2696653a0de
js988174 <76925402+js988174@users.noreply.github.com>
2022-06-08 23:34:17 +0900

    QueryDSL 설정

--- src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -21,4 +21,5 @@ public class NotEnoughStockException extends RuntimeException {
     protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
+
 }
