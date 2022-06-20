ba888b0d911a365a8b7d734d11c830f0e3fd1706
js988174 <76925402+js988174@users.noreply.github.com>
2022-06-20 19:18:50 +0900

    vue.js 오류 수정

---
+++ vue.js/vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        input
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueJsTotoFooter',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style>
+
+</style>
---
+++ vue.js/vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        header
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueJsTodoHeader',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style>
+
+</style>
