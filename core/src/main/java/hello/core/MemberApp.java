cd35a14142123d01b99cc76bc5d93f8956a7773a
js988174 <76925402+js988174@users.noreply.github.com>
2022-07-11 21:48:14 +0900

    Spring으로 변경

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -2,18 +2,24 @@ package hello.core;
 
 import hello.core.member.*;
 import hello.core.order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("findMember = " + findMember.getName());
+        System.out.println("findMembergnj = " + findMember.getName());
         System.out.println("member = " + member.getName());
     }
 }
