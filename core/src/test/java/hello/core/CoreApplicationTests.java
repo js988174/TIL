2e0b905053317a0610bb9b1534018932aac2c4f2
js988174 <76925402+js988174@users.noreply.github.com>
2022-07-13 22:43:05 +0900

    + xml 추가

--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -1,13 +1,23 @@
 package hello.core;
 
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 @SpringBootTest
 class CoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ApplicationContext ac = new
+				GenericXmlApplicationContext("appConfig.xml");
+
+		MemberService memberService = ac.getBean("memberService",
+				MemberService.class);
+		Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
 	}
 
 }
