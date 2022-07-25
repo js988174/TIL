38b1a767444675ff53e7e9c3a631318ebf9b5f11
js988174 <76925402+js988174@users.noreply.github.com>
2022-07-25 21:50:38 +0900

    프로토타입 스코프

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,39 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void protoTypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("prototype.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("prototype.destroy");
+        }
+
+    }
+}
