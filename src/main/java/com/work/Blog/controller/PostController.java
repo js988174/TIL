9d06d26a6f3595c0f22363a6c56161aeeab40a08
js988174 <76925402+js988174@users.noreply.github.com>
2022-09-12 22:58:32 +0900

    Post 데이터

--- src/main/java/com/work/Blog/controller/PostController.java
+++ src/main/java/com/work/Blog/controller/PostController.java
@@ -1,14 +1,19 @@
 package com.work.Blog.controller;
 
 
+import com.work.Blog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PostController {
 
-    @GetMapping
-    public String get() {
+    @GetMapping("/posts")
+    public String post(PostCreate params) {
+        log.info("params={}", params.toString());
         return "Hello World";
     }
 }
