7eea5e4a4b8846f9f0ac2ea9a467b78bb867cf31
js988174 <76925402+js988174@users.noreply.github.com>
2022-09-16 23:16:02 +0900

    Exception Handler 추가

--- project/src/main/java/com/work/Blog/controller/ExceptionController.java
+++ project/src/main/java/com/work/Blog/controller/ExceptionController.java
@@ -1,16 +1,32 @@
 package com.work.Blog.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @ControllerAdvice
+@ResponseBody
 public class ExceptionController {
 
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception.class)
-    public void exceptionHandler() {
+    public Map<String, String> invalidRequestHandler(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getFieldError();
+        String field = fieldError.getField();
+        String message = fieldError.getDefaultMessage();
 
+        Map<String, String> response = new HashMap<>();
+        response.put(field, message);
+        return response;
     }
 }
