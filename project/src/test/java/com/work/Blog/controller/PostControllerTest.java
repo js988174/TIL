f04eb9c3f77c1bae44dd7a87e662910700a2392d
js988174 <76925402+js988174@users.noreply.github.com>
2022-09-18 21:38:59 +0900

    Post test 글 작성

--- project/src/test/java/com/work/Blog/controller/PostControllerTest.java
+++ project/src/test/java/com/work/Blog/controller/PostControllerTest.java
@@ -1,25 +1,28 @@
 package com.work.Blog.controller;
 
+import com.work.Blog.repository.PostRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@AutoConfigureMockMvc
+@SpringBootTest
 class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Test
     @DisplayName("/posts 요청시 Hello World를 출력한다.")
@@ -55,16 +58,16 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 DB값 저장")
     void test3() throws Exception {
-        // expected
+        // when
         mockMvc.perform(get("/posts")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"title\": \"\", \"content\": \"내용입니다.\"}")
+                .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
         )
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.code").value("400"))
-                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
-                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
+                .andExpect(status().isOk())
                 .andDo(print());
+
+        // then
+        Assertions.assertEquals(1L, postRepository.count());
 
     }
 
