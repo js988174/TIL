7dd2c9886f8255157d26db42cc8aa268ba82ac30
js988174 <76925402+js988174@users.noreply.github.com>
2022-09-18 21:24:14 +0900

    Post 글 작성

---
+++ project/src/main/java/com/work/Blog/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.work.Blog.repository;
+
+import com.work.Blog.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
---
+++ project/src/main/java/com/work/Blog/service/PostService.java
@@ -0,0 +1,25 @@
+package com.work.Blog.service;
+
+import com.work.Blog.domain.Post;
+import com.work.Blog.repository.PostRepository;
+import com.work.Blog.request.PostCreate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public void write(PostCreate postCreate) {
+        // postCreate -> Entity
+
+        Post post = new Post(postCreate.getTitle(), postCreate.getContent());
+
+        postRepository.save(post);
+    }
+}
