7f2518d7a34d9f4fed94c6ca634a3d92863f9439
js988174 <76925402+js988174@users.noreply.github.com>
2023-01-07 17:33:00 +0900

    RememberMe 저장 기능

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 * WebSecurityConfigurerAdapter : 웹 보안 기능 초기화 및 설정
 * SecurityConfig: 사용자 정의 보안 설정 클래스
 * HttpSecurity: 세부적인 보안 기능을 설정할 수 있는 API 제공
+
+## 기능
+* rememberMe: 저장 기능
+* RememberMeService에서 토큰 쿠키 추출 -> 존재하지 않으면 chain.doFilter , 존재하면 Decode Token에서 유무 판단
--- src/main/java/io/security/springsecurity/SecurityConfig.java
+++ src/main/java/io/security/springsecurity/SecurityConfig.java
@@ -1,22 +1,30 @@
 package io.security.springsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,21 +39,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("userId")
                 .passwordParameter("passwd")
                 .loginProcessingUrl("/login_proc")
-                .successHandler(new AuthenticationSuccessHandler() {
+                .permitAll();
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
                     @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication" + authentication.getName());
-                        response.sendRedirect("/");
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
                     }
                 })
-                .failureHandler(new AuthenticationFailureHandler() {
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
                     @Override
-                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                        System.out.println("exception" + exception.getMessage());
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
                         response.sendRedirect("/login");
                     }
                 })
-                .permitAll()
-        ;
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService);
+
     }
 }
