d70a950ce2551899408ecc8b55517e10c94a9907
js988174 <76925402+js988174@users.noreply.github.com>
2023-02-03 16:35:38 +0900

    admin, user 권한 설정

---
+++ src/main/java/io/security/springsecurity/repository/RoleHierarchyRepository.java
@@ -0,0 +1,10 @@
+package io.security.springsecurity.repository;
+
+
+import io.security.corespringsecurity.domain.entity.RoleHierarchy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleHierarchyRepository extends JpaRepository<RoleHierarchy, Long> {
+
+    RoleHierarchy findByChildName(String roleName);
+}
