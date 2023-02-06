a4f1c5366c2b1b39e2082da1ed743e305b9eccdf
js988174 <76925402+js988174@users.noreply.github.com>
2023-02-06 15:09:53 +0900

    first commit

---
+++ src/main/java/com/restful/springBoot/Application.java
@@ -0,0 +1,13 @@
+package com.restful.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	// test
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
