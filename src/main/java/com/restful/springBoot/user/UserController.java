c0e2d9cad44c7bf6b9c52d268bd86408e6ec6dd8
js988174 <76925402+js988174@users.noreply.github.com>
2023-02-08 17:26:41 +0900

    Exception Handling

--- src/main/java/com/restful/springBoot/user/UserController.java
+++ src/main/java/com/restful/springBoot/user/UserController.java
@@ -1,8 +1,13 @@
 package com.restful.springBoot.user;
 
 import lombok.NoArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,12 +26,24 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User oneUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
 
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
