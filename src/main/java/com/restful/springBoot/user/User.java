92465796473662618b636415f2d9dac809758c3a
js988174 <76925402+js988174@users.noreply.github.com>
2023-02-09 15:22:11 +0900

    Validation 체크

--- src/main/java/com/restful/springBoot/user/User.java
+++ src/main/java/com/restful/springBoot/user/User.java
@@ -4,8 +4,8 @@ package com.restful.springBoot.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -14,7 +14,9 @@ import java.util.Date;
 public class User {
 
     private Integer id;
+    @Size(min = 2, message = "2글자 이상 입력해 주세요.")
     private String name;
+    @Past
     private Date joinDate;
 
 
